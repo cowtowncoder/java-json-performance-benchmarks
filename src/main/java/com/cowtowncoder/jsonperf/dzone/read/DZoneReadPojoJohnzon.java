ee76ee3cd98bcd0cf55f7260081836ec121d17c9
Cowtowncoder <tatu.saloranta@iki.fi>
2015-08-12 14:36:42 -0700

    Add Johnzon-backend for read test too

---
+++ src/main/java/com/cowtowncoder/jsonperf/dzone/read/DZoneReadPojoJohnzon.java
@@ -0,0 +1,34 @@
+package com.cowtowncoder.jsonperf.dzone.read;
+
+import java.io.ByteArrayInputStream;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.johnzon.mapper.Mapper;
+import org.apache.johnzon.mapper.MapperBuilder;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.State;
+
+import com.cowtowncoder.jsonperf.dzone.MeasurementPOJO;
+
+@State(Scope.Thread)
+@OutputTimeUnit(TimeUnit.SECONDS)
+public class DZoneReadPojoJohnzon extends DZoneReadTestBase
+{
+    protected final Mapper mapper;
+
+    public DZoneReadPojoJohnzon()
+    {
+        mapper = new MapperBuilder().build();
+    }
+
+    @Override
+    public MeasurementPOJO _readItems(byte[] input) throws Exception {
+        return mapper.readObject(new ByteArrayInputStream(input), MeasurementPOJO.class);
+    }
+
+    @Override
+    public MeasurementPOJO _readItems(String input) throws Exception {
+        return mapper.readObject(input, MeasurementPOJO.class);
+    }
+}
