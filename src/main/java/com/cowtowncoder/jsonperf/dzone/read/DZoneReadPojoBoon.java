76590096a1139553e9a16ecc42a9ee4f154ee2f9
Tatu Saloranta <tatu.saloranta@iki.fi>
2015-08-12 22:31:00 -0700

    Add boon read test

---
+++ src/main/java/com/cowtowncoder/jsonperf/dzone/read/DZoneReadPojoBoon.java
@@ -0,0 +1,34 @@
+package com.cowtowncoder.jsonperf.dzone.read;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.State;
+
+import com.cowtowncoder.jsonperf.dzone.MeasurementPOJO;
+
+import org.boon.json.JsonFactory;
+import org.boon.json.ObjectMapper;
+
+@State(Scope.Thread)
+@OutputTimeUnit(TimeUnit.SECONDS)
+public class DZoneReadPojoBoon extends DZoneReadTestBase
+{
+    protected final ObjectMapper mapper;
+
+    public DZoneReadPojoBoon()
+    {
+        mapper = JsonFactory.create();
+    }
+
+    @Override
+    public MeasurementPOJO _readItems(byte[] input) throws Exception {
+        return mapper.readValue(input, MeasurementPOJO.class);
+    }
+
+    @Override
+    public MeasurementPOJO _readItems(String input) throws Exception {
+        return mapper.readValue(input, MeasurementPOJO.class);
+    }
+}
