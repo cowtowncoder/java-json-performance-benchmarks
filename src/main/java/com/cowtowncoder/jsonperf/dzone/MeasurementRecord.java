6c0a309921cfb7af7d6b9c4f3df42b7542fddf16
Tatu Saloranta <tatu.saloranta@iki.fi>
2015-08-13 20:58:24 -0700

    ensure ordering for output, as expected by tests

--- src/main/java/com/cowtowncoder/jsonperf/dzone/MeasurementRecord.java
+++ src/main/java/com/cowtowncoder/jsonperf/dzone/MeasurementRecord.java
@@ -3,48 +3,51 @@ package com.cowtowncoder.jsonperf.dzone;
 import java.io.IOException;
 import java.util.*;
 
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
 /**
  * Main test item. Contains both fields and getters/setters, given that different
  * libraries access data in different ways: some require fields (Gson at least),
  * others getters/setters (Jackson jr); others work with either (Jackson)
  */
+@JsonPropertyOrder({ "measurementId", "type", "duration", "time" })
 public class MeasurementRecord
 {
     public String measurementId;
+    public MeasurementType type;
+
     public long duration;
     public long time;
-
-    public MeasurementType type;
 
     // for deser
     protected MeasurementRecord() { }
     
-    public MeasurementRecord(String measurementId, long duration, long time,
-            MeasurementType type)
+    public MeasurementRecord(String measurementId, MeasurementType type,
+            long duration, long time)
     {
         this.measurementId = measurementId;
+        this.type = type;
         this.duration = duration;
         this.time = time;
-        this.type = type;
     }
 
     public String getMeasurementId() { return measurementId; }
+    public MeasurementType getType() { return type; }
     public long getDuration() { return duration; }
     public long getTime() { return time; }
-    public MeasurementType getType() { return type; }
 
     public void setMeasurementId(String s) { measurementId = s; }
+    public void setType(MeasurementType t) { type = t; }
     public void setDuration(Long l) { duration = l; }
     public void setTime(long l) { time = l; }
-    public void setType(MeasurementType t) { type = t; }
 
     public static List<MeasurementRecord> construct(int count)
     {
         List<MeasurementRecord> result = new ArrayList<>(count);
         long now = System.currentTimeMillis();
         for (int i = 0; i < count; ++i) {
-            result.add(new MeasurementRecord("/test.html",
-                    10, now+i, MeasurementType.WEB_REQUEST));
+            result.add(new MeasurementRecord("/test.html", MeasurementType.WEB_REQUEST,
+                    10, now+i));
         }
         return result;
     }
@@ -54,10 +57,10 @@ public class MeasurementRecord
     public void appendAsJSON(Appendable target) throws IOException
     {
         target.append(String.format("{\"measurementId\":\"%s\","
+                +"\"type\":\"%s\","
                 +"\"duration\":%d,"
-                +"\"time\":%d,"
-                +"\"type\":\"%s\"}",
-                measurementId, duration, time, type.name()
+                +"\"time\":%d }",
+                measurementId, type.name(), duration, time
                 ));
     }
 }
