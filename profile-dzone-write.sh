138fac7f7ab57b4d2e36b193be7224b88e4ea431
Tatu Saloranta <tatu.saloranta@iki.fi>
2016-07-28 08:34:00 -0700

    ...

--- profile-dzone-write.sh
+++ profile-dzone-write.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-java -Xmx256m -server -Xrunhprof:cpu=samples,depth=10,verbose=n,interval=2  -jar target/microbenchmarks.jar ".*DZoneWrite.*write1k.*AsString.*" -wi 7 -i 30 -f 1
+java -Xmx256m -server -Xrunhprof:cpu=samples,depth=10,verbose=n,interval=2  -jar target/microbenchmarks.jar ".*DZoneWriteJackson2x.write1kUsingStream.*" -wi 5 -w 2 -i 10 -r 5 -f 1 -t 1
+
